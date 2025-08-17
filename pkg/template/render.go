0b5eeb8d050e1f069bfbff2c7fbd3e9fe071c118
Babafemi Jimoh-Kuku <babafemijk@gmail.com>
2025-08-18 00:48:05 +0100

    complete basic render function

--- pkg/template/render.go
+++ pkg/template/render.go
@@ -1,3 +1,28 @@
 package template
 
-func Render(tmpl string, data map[string]string) (string, error) {}
+import (
+	"regexp"
+)
+
+func Render(tmpl string, data map[string]string) (string, error) {
+	pattern := `{{\s*([a-zA-Z]+)\s*}}`
+
+	re, err := regexp.Compile(pattern)
+	if err != nil {
+		return "", err
+	}
+
+	out := re.ReplaceAllStringFunc(tmpl, func(m string) string {
+		sub := re.FindStringSubmatch(m)
+		if len(sub) == 2 {
+			key := sub[1]
+			if val, ok := data[key]; ok {
+				return val
+			}
+		}
+
+		return m
+	})
+
+	return out, nil
+}
